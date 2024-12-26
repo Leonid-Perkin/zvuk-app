4583a21bfb680ff951b15c61cb0629a0c3e5ac6e
Leonid <148010246+Leonid-Perkin@users.noreply.github.com>
2024-12-26 20:29:22 +0300

    Добавить начальную структуру проекта с HTML и основным скриптом для Electron

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+    icon: __dirname + '/icon.png',
+  });
+
+  mainWindow.loadURL('https://zvuk.com');
+  Menu.setApplicationMenu(null);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
